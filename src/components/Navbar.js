7c1e0032bb88a88d56a8e5bfe4b681808e3d4f9f
umang <106922225+umang-soni@users.noreply.github.com>
2023-09-26 23:45:08 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React ,{useState}from 'react'
+import { Link } from 'react-router-dom'
+import Badge from 'react-bootstrap/Badge'
+import Modal from '../Modal'
+import Cart from '../screens/Cart'
+export default function Navbar() {
+  const[cartView,setCartView]=useState(false)
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+  <div className="container-fluid">
+    <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav me-auto mb-2">
+        <li className="nav-item">
+          <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
+        </li>
+        
+      </ul>
+      <div className='d-flex'>
+      <Link className="btn bg-white text-success mx-1" to="/login">Log Out</Link>
+          
+      <Link className="btn bg-white text-success mx-1" onClick={()=>{setCartView(true)}}>My Cart{" "}
+      <Badge pill bg='danger'>2</Badge>
+      </Link>
+      {cartView? <Modal onClose={()=>setCartView(false)}><Cart></Cart> </Modal>:null}
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
